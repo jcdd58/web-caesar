d0b25732a9f82ea6e71dfc62ed8c825d5fa365cb
Jamie Roberts <roberts.jamiecarol@gmail.com>
2017-01-26 15:59:07 -0600

    Added cipher

--- main.py
+++ main.py
@@ -16,9 +16,76 @@
 #
 import webapp2
 
+page_header = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Caesar</title>
+    <style type="text/css">
+        .error {
+            color: red;
+        }
+    </style>
+</head>
+<body>
+    <h1>
+        <a href="/">Caesar</a>
+    </h1>
+"""
+
+# html boilerplate for the bottom of every page
+page_footer = """
+</body>
+</html>
+"""
+
+
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        encrypted = self.request.get("encrypted")
+
+        ceasar_form = """
+            <form method="POST">
+                <label for="rot">Rotation: </label>
+                <input type="text" name="rot" id="rot"><br><br>
+                <label for="text">Text to encrypt:</label><br>
+                <textarea name="text" id="text" style="height:200px;width:400px;">{}</textarea><br><br>
+                <input type="submit" value="Submit">
+            </form>
+        """.format(encrypted)
+
+        self.response.write(page_header + ceasar_form + page_footer)
+
+    def post(self):
+        def alphabet_position(char):
+        	if char.isupper():
+        		return ord(char) - 65
+        	if char.islower():
+        		return ord(char) - 97
+
+        def rotate_character(char, rot):
+        	if not char.isalpha():
+        		return char
+        	if char.isupper():
+        		return chr(((alphabet_position(char) + rot) % 26) + 65)
+        	if char.islower():
+        		return chr(((alphabet_position(char) + rot) % 26) + 97)
+
+        def encrypt(text,rot):
+        	encrypted_text = ""
+        	for char in text:
+        		if char == " ":
+        			encrypted_text += char
+        		else: encrypted_text += rotate_character(char,int(rot))
+        	return encrypted_text
+
+        rot = self.request.get("rot")
+        text = self.request.get("text")
+
+        encrypted_text = encrypt(text, rot)
+
+        self.redirect("/?encrypted={}".format(encrypted_text))
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
